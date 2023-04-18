ec4581dbe7b51881c46678b7b49caa5d34f5491a
Corotyest <74837790+Corotyest@users.noreply.github.com>
2023-04-18 13:17:30 -0600

    Added authorization to token.

--- libs/species/token.lua
+++ libs/species/token.lua
@@ -8,10 +8,13 @@ local TokenTypes = {
     bearer = 'Bearer'
 }
 
+local AccessForm = '%s %s'
+
 local Token = class('Token', Component)
 local getters, setters = Token.__getters, Token.__setters
 
 function Token:__init()
+    self.__access_form = AccessForm
     self.__token_types = TokenTypes
 
     local tokens = class 'Tokens'
@@ -28,6 +31,8 @@ end
 function Token:call(data)
     local len = self.__tokens:getn()
     local token = class(format('Token #%s', len), self.__super_token)
+
+    token.__access_form = self.__access_form
     token.__token_types = self.__token_types
 
     local specials = self.__specials
@@ -63,6 +68,15 @@ end
 
 function getters.access(self)
     return self.__access_token or self.id
+end
+
+function getters.authorization(self)
+    local type = self:getTokenType()
+    if not type then
+        return nil
+    end
+
+    return format(self.__access_form, type, self.access)
 end
 
 return Token
