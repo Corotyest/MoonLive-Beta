01199c341c9da390cc47696091c43cd8bae90662
Corotyest <74837790+Corotyest@users.noreply.github.com>
2023-04-20 13:09:52 -0600

    The component now should do their work.

--- libs/species/types/component.lua
+++ libs/species/types/component.lua
@@ -1,7 +1,25 @@
 local class = require 'class'
 
-local component = class 'Component'
+local component = class 'Component'()
 local getters, setters = component.__getters, component.__setters
+
+local props = {
+    __getters = getters,
+    __setters = setters
+}
+
+component[class.events.call] = function(self, element, ...)
+    local obj = element
+    if not class.isClass(element) then
+        obj = class(element, ...)
+    end
+
+    for name, property in pairs(props) do
+        property:__clone(true, { protected = true }, obj[name])
+    end
+
+    return obj
+end
 
 function getters.id(self)
     return self.__id
