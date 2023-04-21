4072754603cf438980a42fc81969f6291ddc3773
Corotyest <74837790+Corotyest@users.noreply.github.com>
2023-04-21 01:25:55 -0600

    feat testing of the main beta API.

---
+++ feat.lua
@@ -0,0 +1,42 @@
+local moonLive = require './init'()
+
+local client = moonLive.client
+
+print(moonLive, client)
+
+
+local token, msg = moonLive:requestToken()
+
+print(token, 'message:', msg)
+
+client.id = 'yycm39ixe2qhxlmuhzoyj9meopv4zt'
+client.secret = 'fdk4hgbdarwqpo11nnt7kekpvhye6s'
+
+-- print(moonLive.clientId)
+
+local token, msg = moonLive:requestToken()
+
+print(token, msg)
+
+print(token.authorization)
+
+local user, msg = moonLive:getUser 'corotyest'
+print(user, msg)
+
+-- for k, v in user:__iter { raw = true } do
+--     p(k, v)
+-- end
+
+print(user.id, user.__id)
+
+local res, msg = moonLive:getChannel()
+print(res, msg)
+
+-- print(moonLive:getChannel(user))
+
+-- local res, msg = moonLive:getChannel '0000000121212121212121212'
+-- print(res, msg)
+
+local channel = moonLive:getChannel(user)
+
+print(channel, channel.name)
